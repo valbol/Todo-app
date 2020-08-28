51f6bd78b882963cbcad6a660c9c3554e1858abd
VALERYBOLTAVSKY <valerybo@il.ibm.com>
2020-08-29 00:14:06 +0300

    refactoring promises to async await

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -1,88 +1,83 @@
 const { db } = require('../util/admin');
 
-exports.getAllTodos = (request, response) => {
-  db.collection('todos')
-    .orderBy('createdAt', 'desc')
-    .get()
-    .then(data => {
-      let todos = [];
-      data.forEach(doc => {
-        todos.push({
-          todoId: doc.id,
-          title: doc.data().title,
-          body: doc.data().body,
-          createdAt: doc.data().createdAt,
-        });
+exports.getAllTodos = async (request, response) => {
+  let data = await db.collection('todos').orderBy('createdAt', 'desc').get();
+  let todos = [];
+  try {
+    data.forEach(doc => {
+      todos.push({
+        todoId: doc.id,
+        title: doc.data().title,
+        body: doc.data().body,
+        createdAt: doc.data().createdAt,
       });
-      return response.json(todos);
-    })
-    .catch(err => {
-      console.log(err);
-      return response.status(500).json({ error: err.code });
     });
+    return response.json(todos);
+  } catch (err) {
+    console.log(err);
+    return response.status(500).json({ error: err.code });
+  }
 };
 
-exports.postOneTodo = (request, response) => {
+exports.postOneTodo = async (request, response) => {
   if (request.body.body.trim() === '') {
-    return response.status(400).json({ body: 'Must not be empty' });
+    return response.status(400).json({ body: 'Body ust not be empty' });
   }
-  if (request.body.title.trim === '') {
-    return response.status(400).json({ title: 'Must not be empty' });
+
+  if (request.body.title.trim() === '') {
+    return response.status(400).json({ title: 'Title must not be empty' });
   }
+
   const newTodoItem = {
     title: request.body.title,
     body: request.body.body,
     createdAt: new Date().toISOString(),
   };
-  db.collection('todos')
-    .add(newTodoItem)
-    .then(doc => {
-      const responseTodoItem = newTodoItem;
-      responseTodoItem.id = doc.id;
-      return response.json(responseTodoItem);
-    })
-    .catch(err => {
-      response.status(500).json({ error: 'something went wrong' });
-      console.log(err);
-    });
+
+  let result = await db.collection('todos').add(newTodoItem);
+  try {
+    const responseTodoItem = newTodoItem;
+    responseTodoItem.id = result.id;
+    return response.json(responseTodoItem);
+  } catch (err) {
+    response.status(500).json({ error: 'Something went wrong' });
+    console.log(err);
+  }
 };
 
-exports.deleteTodo = (request, response) => {
+exports.deleteTodo = async (request, response) => {
   const document = db.doc(`/todos/${request.params.todoId}`);
-  document
-    .get()
-    .then(doc => {
-      if (!doc.exists) {
-        console.log(request.params.todoId);
-        return response
-          .status(404)
-          .json({ error: `Todo item not found - ${request.params.todoId}` });
-      }
-      return document.delete();
-    })
-    .then(() => {
-      response.json({ message: 'Delete successfull' });
-    })
-    .catch(err => {
-      console.log(err);
-      return response.status(500).json({ error: err.code });
-    });
+  let todoItem = await document.get();
+
+  if (!todoItem.exists) {
+    console.log(request.params.todoId);
+    return response
+      .status(404)
+      .json({ error: `Todo item not found - id=${request.params.todoId}` });
+  }
+  try {
+    let result = await document.delete();
+    response.json({ message: 'Delete successfull' });
+    return result;
+  } catch (err) {
+    console.log(err);
+    return response.status(500).json({ error: err.code });
+  }
 };
 
-exports.editTodo = (request, response) => {
+exports.editTodo = async (request, response) => {
   if (request.body.todoId || request.body.createdAt) {
     return response
       .status(403)
       .json({ message: 'Not allowed to edit the field' });
   }
-  const document = db.collection('todos').doc(`${request.params.todoId}`);
-  document
-    .update(request.body)
-    .then(() => {
-      response.json({ message: 'Updated successfully' });
-    })
-    .catch(err => {
-      console.log(err);
-      return response.status(500).json({ error: err.code });
-    });
+  try {
+    const document = db.collection('todos').doc(`${request.params.todoId}`);
+    let updatedTodo = await document.update(request.body);
+    response.json({ message: 'Updated successfully' });
+    return updatedTodo;
+  } catch (err) {
+    console.log(err);
+    return response.status(500).json({ error: err.code });
+  }
 };
