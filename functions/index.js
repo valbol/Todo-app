9610f9513a41de66675d2a32ad45746c00ebb77a
VALERYBOLTAVSKY <valerybo@il.ibm.com>
2020-09-04 01:46:20 +0300

    created getOneTodo AND secured the app adding the auth to all methods

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,17 @@
 const functions = require('firebase-functions');
+
 const app = require('express')();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info('Hello logs!', { structuredData: true });
-//   response.send('Hello from Firebase!');
-// });
+const auth = require('./util/auth');
 
 const {
     getAllTodos,
+    getOneTodo,
     postOneTodo,
     deleteTodo,
     editTodo,
 } = require('./APIs/todos');
 
-const auth = require('./util/auth');
 const {
     loginUser,
     signUpUser,
@@ -25,11 +20,12 @@ const {
     updateUserDetails,
 } = require('./APIs/users');
 
-//CRUD
-app.get('/todos', getAllTodos);
-app.post('/todo', postOneTodo);
-app.delete('/todo/:todoId', deleteTodo);
-app.put('/todo/:todoId', editTodo);
+//CRUD - TODOs
+app.get('/todos', auth, getAllTodos);
+app.get('/todo/:todoId', auth, getOneTodo);
+app.post('/todo', auth, postOneTodo);
+app.delete('/todo/:todoId', auth, deleteTodo);
+app.put('/todo/:todoId', auth, editTodo);
 
 //Users
 app.post('/login', loginUser);
